c12d6bad93aceab5e24440e0b4e73117337a9148
Momin Karim <mominkarim0305@gmail.com>
2023-08-10 09:04:36 +0500

    first
    Signed-off-by: Momin Karim <mominkarim0305@gmail.com>

---
+++ fluttertest/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'thirdpage.dart';
+import 'firstpage.dart';
+import 'secondpage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const Home(),
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: Colors.indigo[900],
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.indigo[900],
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                text: "original \nimage",
+              ),
+              Tab(
+                text: "Scan \nimage",
+              ),
+              Tab(
+                text: "Draw \nimage",
+              ),
+            ],
+          ),
+          title: const Text(
+            'Make PDF',
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Firstpage(),
+            Secondpage(),
+            Thirdpage(),
+          ],
+        ),
+      ),
+    );
+  }
+}
